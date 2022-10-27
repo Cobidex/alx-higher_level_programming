52d13c098cf634a0c536ce4fa6e2045fe1ebe344
Stephen <stevobialor@gmail.com>
2022-10-27 22:36:25 +0000

    done

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""Square generation module for Python project 0x06
+"""
+
+
 class Square:
-    pass
+        """class defined for square generation
+            """
+                pass
