289a7b0a837dd1257c137cd06a8f6b240f175de8
Stephen <stevobialor@gmail.com>
2022-10-27 23:05:15 +0000

    done

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module will create a class"""
+
+
+class Square():
+    """"a class of type square"""
+    def __init__(self, size=0):
+        """initializing quare object"""
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """calculates the area of a square object"""
+        return self.__size * self.__size
