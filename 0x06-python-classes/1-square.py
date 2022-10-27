ee01b9b80da3f64fe2000902bad849ad010498f6
Stephen <stevobialor@gmail.com>
2022-10-27 22:47:51 +0000

    done

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module creates a new class Square"""
 
+
 class Square():
-        """A square class."""
-        def __init__(self, size):
-            """Initialization square."""
-            self.__size = size
+    """A square class."""
+    def __init__(self, size):
+        """Initialization square."""
+        self.__size = size
