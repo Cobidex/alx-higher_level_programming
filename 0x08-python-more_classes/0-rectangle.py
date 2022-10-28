3de4997923138aa7ac0af70cd02708b2c09a26b5
Stephen <stevobialor@gmail.com>
2022-10-28 18:03:44 +0000

    done

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A module to contain class of rectangle type"""
+
+
+class Rectangle():
+    """class of type rectangle"""
+
+    def __init__(self):
+        """initializing class object"""
+
+        pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This folder will contain more python class task scripts
