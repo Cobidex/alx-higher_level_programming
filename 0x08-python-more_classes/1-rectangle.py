5f8c74e6708b38524ed040349a40bcf8add183ec
Stephen <stevobialor@gmail.com>
2022-10-28 19:08:58 +0000

    done

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,80 +1,64 @@
 #!/usr/bin/python3
-"""A module to contain class of rectangle type"""
+"""""This module create a rectangle"""
 
 
-class Rectangle():
-    """class of type rectangle"""
-
+class Rectangle:
+    """
+    class that generate a rectangle
+    """
     def __init__(self, width=0, height=0):
-        """constructor initializing class object
-
-        Arguments:
-            width{int} -- width of rectangle (default: 0)
-            height{int} -- height of rectangle (default: 0)
+        """Constructor initialize the class rectangle
+        Keyword Arguments:
+            width {int} -- width of the rectangle (default: {0})
+            height {int} -- height of the rectangle (default: {0})
         """
-
-        if type(width) != int:
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
-        elif height < 0:
-            raise ValueError("width must be >= 0")
-        elif type(height) != int:
-            raise TypeError("width must be an integer")
-        else:
-            self.__height = height
-            self.__width = width
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """Getter function of width
-
         Returns:
-            int -- width of rectangle
+            int -- width fo the rectangle
         """
         return self.__width
 
-    @property
-    def height(self):
-        """Getter function of height
-
-        Returns:
-            height of rectangle
-        """
-        return self.__height
-
     @width.setter
     def width(self, value):
-        """setter function of width
-
+        """Setter function to width
         Arguments:
-            value{int} -- value of width
-
+            value {int} -- [valeu of width]
         Raises:
-            TypeError: width must be of type int
-            ValueError: width must be >=0
+            TypeError: width must be an integer
+            ValueError: width must be >= 0
         """
         if type(value) != int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter function to height
+        Returns:
+            int -- height of the rectangle
+        """
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """setter function of width
-
-            Arguments:
-                value{int} -- value of width
-
-            Raises:
-                TypeError: width must be of type int
-                ValueError: width must be >=0
+        """Setter function os height
+        Arguments:
+            value {int} -- Value of height
+        Raises:
+            TypeError: height must be an integer
+            ValueError: height must be >= 0
         """
         if type(value) != int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+
+        self.__height = value
