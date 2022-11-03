2c14a255fbc8c2b325c8cdf34be6d665939ba4f9
Stephen <stevobialor@gmail.com>
2022-11-03 17:29:45 +0000

    replace attributes

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''module for a class of student'''
+
+
+class Student():
+    '''creates a class of type student'''
+
+    def __init__(self, first_name, last_name, age):
+        '''initializes the class object'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''retrieves a dictionary rep of class object'''
+        if attrs is None or type(attrs) != list:
+            return self.__dict__
+        else:
+            temp = {}
+            for item in attrs:
+                if not isinstance(item, str):
+                    return self.__dict__
+                if item in self.__dict__.keys():
+                    temp[item] = self.__dict__[item]
+            return temp
+
+    def reload_from_json(self, json):
+        '''replaces class attributes with json dict'''
+
+        self.__dict__ = json
