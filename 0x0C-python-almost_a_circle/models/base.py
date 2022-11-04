e4faef584539e3f394d0d12e73903fb17155fc12
Stephen <stevobialor@gmail.com>
2022-11-04 06:02:55 +0000

    model and init files

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''a module to hold base class definition'''
+
+
+class Base():
+    '''class of Base'''
+
+    __nb_objects = 0
+    '''private attribute'''
+
+    def __init__(self, id=None):
+        '''initializing class object'''
+
+        if id is not None:
+            self.id = id
+        else:
+            self.__nb_objects += 1
+            self.id = self.__nb_objects
+
