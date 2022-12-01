01f33a6a8a8d0e4c6846b6ed3ea111f138716f72
Stephen <stevobialor@gmail.com>
2022-12-01 17:26:44 +0000

    done

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,2 @@
 -- computes average score of all records in second_table
-SELECT AVG(score) AS average FROM second_table
-GROUP BY city
-ORDER BY temperature DESC;
+SELECT AVG(score) AS average FROM second_table;
