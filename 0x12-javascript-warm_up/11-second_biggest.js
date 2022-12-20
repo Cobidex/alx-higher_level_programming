4c0f87491430729580b0a3a67a02dbdec54b3358
Stephen <stevobialor@gmail.com>
2022-12-20 20:25:39 +0000

    done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,12 +4,12 @@ const len = process.argv.length;
 if (len < 4) {
   console.log(0);
 } else {
-  const my_arg = [];
+  const myArg = [];
   let i = 2;
   while (i < process.argv.length) {
-    my_arg.push(parseInt(process.argv[i]));
+    myArg.push(parseInt(process.argv[i]));
     i++;
   }
-  const sorted_args = my_arg.sort((a, b) => (b - a));
-  console.log(sorted_args[1]);
+  const sortedArgs = myArg.sort((a, b) => (b - a));
+  console.log(sortedArgs[1]);
 }
