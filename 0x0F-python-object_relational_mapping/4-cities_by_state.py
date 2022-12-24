afb68220bfd7ff1ce50cdd1adfbf419263a47486
Stephen <stevobialor@gmail.com>
2022-12-24 15:36:13 +0000

    done

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""takes in arguments and displays all values"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+
+    # Use a parameterized query to protect against MySQL injection attacks
+    query = "SELECT * FROM cities ORDER BY id ASC"
+    cursor.execute(query)
+
+    # Fetch and print the results
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
