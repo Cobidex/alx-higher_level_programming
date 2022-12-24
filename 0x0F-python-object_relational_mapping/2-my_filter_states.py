ae6f8221ec454ce74103b381762e88c8deeccf78
Stephen <stevobialor@gmail.com>
2022-12-24 11:52:17 +0000

    done

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""List all states with a name matching the argument from the database hbtn_0e_0_usa"""
+"""List all states with a name matching the argument hbtn_0e_0_usa"""
 
 import sys
 import MySQLdb
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     # Select all states with a name matching the argument, sorted by id
     query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
-    cur.execute(query, (sys.argv[4],))
+    cur.execute(query, (sys.argv[4]))
 
     # Print the states
     rows = cur.fetchall()
