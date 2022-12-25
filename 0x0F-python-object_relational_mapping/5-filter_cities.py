2f390a84ef753ab42c001189cc40b412d3fa4339
Stephen <stevobialor@gmail.com>
2022-12-25 12:44:13 +0000

    done

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,36 +1,22 @@
 #!/usr/bin/python3
-
+# Displays all cities of a given state from the
+# states table of the database hbtn_0e_4_usa.
+# Safe from SQL injections.
+# Usage: ./5-filter_cities.py <mysql username> \
+#                             <mysql password> \
+#                             <database name> \
+#                             <state name searched>
 import sys
 import MySQLdb
 
-if "__name__" == "__main__":
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
 
-    # Create cursor
-    cursor = db.cursor()
-
-    # Execute SQL query
-    cursor.execute("
-        SELECT c.name
-        FROM states AS s
-        INNER JOIN cities AS c
-        ON s.id = c.state_id
-        WHERE s.name = %s
-        ORDER BY c.id ASC
-    ", (sys.argv[4],))
-
-    # Print results
-    results = cursor.fetchall()
-    if results:
-        print(", ".join([result[0] for result in results]))
-
-    # Close cursor and connection
-    cursor.close()
+    c.close()
     db.close()
