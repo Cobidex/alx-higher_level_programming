8afa87269ebd9bef9b8a7f10e4ab8045ada21e02
Stephen <stevobialor@gmail.com>
2022-12-26 17:50:48 +0000

    done

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""contains the state class"""
+"""contains city class"""
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 from model_state import Base, State
 
-
 class City(Base):
-    """Represents a state for a MySQL database.
-    __tablename__ (str): The name of the MySQL table to store States.
-    id (sqlalchemy.Integer): The state's id.
-    name (sqlalchemy.String): The state's name.
-    """
     __tablename__ = 'cities'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
