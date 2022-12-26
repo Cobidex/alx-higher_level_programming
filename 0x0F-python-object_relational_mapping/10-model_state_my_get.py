6d8338bb318f2efd9da26de3c2b60244dec98e7d
Stephen <stevobialor@gmail.com>
2022-12-26 10:59:01 +0000

    done git push origin master

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""lists all State objects from the database hbtn_0e_6_usa"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    u_name = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(u_name, pwd, db))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    data = session.query(State).all()
+    for datum in data:
+        if (datum.name == sys.argv[4]):
+            print(datum.id)
+            session.close()
+            return
+    printf('Not found')
+
+    session.close()
