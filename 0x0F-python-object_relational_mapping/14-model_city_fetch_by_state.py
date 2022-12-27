0defd7f360ed1fae9690a54a62295491208ecdfd
Stephen <stevobialor@gmail.com>
2022-12-27 06:51:07 +0000

    done

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""prints all City objects from the database hbtn_0e_14_usa"""
+"""
+This script defines a City class
+to work with MySQLAlchemy ORM.
+"""
 
-import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_city import City
+from sqlalchemy import Column, Integer, String, ForeignKey
 
-if __name__ == "__main__":
-    uname = sys.argv[1]
-    pwd = sys.argv[2]
-    db = sys.argv[3]
 
-    link = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-    engine = create_engine(link. format(uname, pwd, db))
+class City(Base):
+    """City class
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The id of the class
+        name (str): The name of the class
+        state_id (int): The state the city belongs to
+    """
+    __tablename__ = 'cities'
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    cities = session.query(City).order_by(City.id).all()
-
-    for city in cities:
-        print('{}: ({}) {}'. format(city.state.name, city.id, city.name))
-
-    session.close()
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
