3be91ecf5700b9a4bbfc360b0c7958f890035f4b
Stephen Obialor <stevobialor@gmail.com>
2023-01-16 13:55:22 +0000

    done

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays all HTTP methods the server will accept
-curl -s GET "$1" | grep "Allow" | cut -d " " -f2 | sed 's/,/, /g'
+curl -s get "$1" | grep "Allow" | cut -d " " -f2 | sed 's/,/, /g'
