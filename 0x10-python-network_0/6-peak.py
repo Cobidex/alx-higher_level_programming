8ffabc375a98590fc43d74bc574bfd080eb02025
Stephen Obialor <stevobialor@gmail.com>
2023-01-16 22:02:27 +0000

    done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python3
-'''finds the peak element'''
+'''finds a peak in a list of integers'''
 
 
 def find_peak(list_of_integers):
-    '''finds the peak element'''
-
+    '''function tto find the peak'''
     if len(list_of_integers) == 0:
         return None
+
     if len(list_of_integers) == 1:
         return list_of_integers[0]
-    peak = list_of_integers[0]
-    for i in range(0, len(list_of_integers) - 1):
-        if list_of_integers[i] <= list_of_integers[i + 1]:
-            peak = list_of_integers[i + 1]
-    return peak
+
+    mid = (len(list_of_integers) - 1) // 2
+
+    if mid == 0:
+        if list_of_integers[mid] >= list_of_integers[mid + 1]:
+            return list_of_integers[mid]
+        else:
+            return list_of_integers[mid + 1]
+
+    if mid == (len(list_of_integers) - 1):
+        if list_of_integers[mid] >= list_of_integers[mid - 1]:
+            return list_of_integers[mid]
+        else:
+            return list_of_integers[mid - 1]
+
+    left = find_peak(list_of_integers[:mid])
+    right = find_peak(list_of_integers[mid:])
+
+    if left < right:
+        return right
+    else:
+        return left
