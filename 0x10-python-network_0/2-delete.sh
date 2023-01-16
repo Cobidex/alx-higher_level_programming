46cd705a04600cafd41d7fa22634c402ae5c6074
Stephen Obialor <stevobialor@gmail.com>
2023-01-16 13:13:08 +0000

    done

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -s DELETE "$1"
+# sends a DELETE request to the URL passed as the first argument and display
+curl -sX DELETE "$1"
