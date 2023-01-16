54e62c41d1a60c84b75de74d75c34a9b2480fac5
Stephen Obialor <stevobialor@gmail.com>
2023-01-16 21:28:58 +0000

    done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,6 +7,9 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return None
 
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
     mid = (len(list_of_integers) - 1) // 2
 
     if mid == 0:
@@ -14,23 +17,17 @@ def find_peak(list_of_integers):
             return list_of_integers[mid]
         else:
             return list_of_integers[mid + 1]
+
     if mid == (len(list_of_integers) - 1):
         if list_of_integers[mid] >= list_of_integers[mid - 1]:
             return list_of_integers[mid]
         else:
             return list_of_integers[mid - 1]
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
 
-    left = find_peak(list_of_integers[:mid + 1])
-    right = find_peak(list_of_integers[mid + 1:])
-    if left is None and right is not None:
-        return right
-    if right is None and left is not None:
-        return left
-    if right is None and left is None:
-        return None
+
+    left = find_peak(list_of_integers[:mid])
+    right = find_peak(list_of_integers[mid:])
+
     if left < right:
         return right
     else:
