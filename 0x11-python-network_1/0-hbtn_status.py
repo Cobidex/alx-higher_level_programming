37bfeb11d6d548bd2319119b305cb53affedf07d
Stephen Obialor <stevobialor@gmail.com>
2023-01-17 11:37:23 +0000

    done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-'''module for making request'''
+'''
+module for making request
+'''
 
 import urllib.request
 
 
 if __name__ == "__main__":
+
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         html = response.read()
