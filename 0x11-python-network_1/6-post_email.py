9a4e6a539a81c457e98836a40cb93f34a7945405
Stephen Obialor <stevobialor@gmail.com>
2023-01-17 14:21:31 +0000

    done

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+    script that takes in a URL and an email address,
+    sends a POST request to the passed URL with the email
+    as a parameter, and finally displays the body of the response
+'''
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = sys.argv[2]
+    my_dict = {'email': value}
+
+    req = requests.post(url, data=my_dict)
+    print(req.text)
