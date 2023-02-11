4d9446d03ee8f0e214f2d25e39c76aae163f2b00
Stephen Obialor <stevobialor@gmail.com>
2023-02-11 23:06:31 +0000

    done

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-request(process.argv[2], function (ero, res) {
-  console.log('code:', res.statusCode); // Print response status code if response was received
+request.get(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(`code: ${response.statusCode}`);
 });
