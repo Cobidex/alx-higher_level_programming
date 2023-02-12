152a2c1de5b03d1f5e0c4f9f807602b14ce352c7
Stephen Obialor <stevobialor@gmail.com>
2023-02-12 10:12:16 +0000

    done

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,6 +2,7 @@
 
 const request = require('request');
 const url = process.argv[2];
+let num = 0;
 request.get(url, function (error, response, body) {
   if (error) {
     console.log(error);
@@ -12,12 +13,13 @@ request.get(url, function (error, response, body) {
     return;
   }
   const res = JSON.parse(body);
-  let num = 0;
-  for (let i = 0; i < res.length; i++) {
-    const characters = res[i].characters;
-    for (let j = 0; j < characters.length; j++) {
-      const character = character.split('/')[-1];
-      if (character === '18') {
+  const movies = res.results;
+  for (let i = 0; i < movies.length; ++i) {
+    const characters = movies[i].characters;
+    for (let j = 0; j < characters.length; ++j) {
+      const character = characters[j];
+      const characterId = character.split('/')['5'];
+      if (characterId === '18') {
         num += 1;
       }
     }
